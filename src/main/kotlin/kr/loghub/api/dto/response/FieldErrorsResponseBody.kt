84d2168696ca7701f28639995d2cde500bf73979
gymynnym <gymynnym@gmail.com>
2025-04-23 23:17:58 +0900

    feat(response): Implement standardized ResponseBody data classes

---
+++ src/main/kotlin/kr/loghub/api/dto/response/FieldErrorsResponseBody.kt
@@ -0,0 +1,8 @@
+package kr.loghub.api.dto.response
+
+import org.springframework.http.HttpStatus
+
+data class FieldErrorsResponseBody(
+    val fieldErrors: Map<String, String>,
+    override val status: HttpStatus
+) : ResponseBody(status = status)
---
+++ src/main/kotlin/kr/loghub/api/dto/response/ResponseBody.kt
@@ -0,0 +1,14 @@
+package kr.loghub.api.dto.response
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import org.springframework.http.HttpStatus
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+abstract class ResponseBody(
+    val timestamp: String = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME),
+    open val status: HttpStatus,
+) {
+    @get:JsonProperty("code")
+    val code: Int get() = status.value()
+}
