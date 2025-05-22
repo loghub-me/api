d89d17992c96f4e24ff349ce633fe7cfa0166495
gymynnym <gymynnym@gmail.com>
2025-05-22 21:05:39 +0900

    fix(auth): Specify UTF-8 charset in JoinTokenRedisSerializer

--- src/main/kotlin/kr/loghub/api/lib/redis/JoinTokenRedisSerializer.kt
+++ src/main/kotlin/kr/loghub/api/lib/redis/JoinTokenRedisSerializer.kt
@@ -5,7 +5,7 @@ import org.springframework.data.redis.serializer.RedisSerializer
 
 class JoinTokenRedisSerializer : RedisSerializer<JoinTokenDTO> {
     override fun serialize(value: JoinTokenDTO?): ByteArray? =
-        value?.let { "${it.otp},${it.email},${it.username},${it.nickname}".toByteArray() }
+        value?.let { "${it.otp},${it.email},${it.username},${it.nickname}".toByteArray(Charsets.UTF_8) }
 
     override fun deserialize(bytes: ByteArray?): JoinTokenDTO? {
         if (bytes == null) return null
