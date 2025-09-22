80516fa12eac2e170ee610dc11295c6ea0022c80
gymynnym <gymynnym@gmail.com>
2025-09-22 23:08:27 +0900

    refactor(auth): Add reserved username check and Allow single space in nickname

--- src/main/kotlin/me/loghub/api/constant/validation/RegexExpression.kt
+++ src/main/kotlin/me/loghub/api/constant/validation/RegexExpression.kt
@@ -2,6 +2,8 @@ package me.loghub.api.constant.validation
 
 object RegexExpression {
     const val USERNAME = "[a-zA-Z0-9]+$"
-    const val NICKNAME = "^[a-zA-Z0-9가-힣]+$"
+    const val USERNAME_RESERVED =
+        "^(?!(?:root|admin|search|post|edit|join|login|logout|legal|safe-link|setting|settings|support|topic|topics)$).*"
+    const val NICKNAME = "^[a-zA-Z0-9가-힣]+(?: [a-zA-Z0-9가-힣]+)*$"
     const val THUMBNAIL = "^(default|[0-9]+)/[a-zA-Z0-9_-]+\\.webp$"
 }
--- src/main/kotlin/me/loghub/api/dto/auth/JoinRequestDTO.kt
+++ src/main/kotlin/me/loghub/api/dto/auth/JoinRequestDTO.kt
@@ -16,6 +16,7 @@ data class JoinRequestDTO(
     @field:NotBlank(message = "유저네임은 필수 입력 항목입니다.")
     @field:Size(min = 4, max = 12, message = "유저네임은 4자 이상 12자 이하이어야 합니다.")
     @field:Pattern(regexp = RegexExpression.USERNAME, message = "유저네임은 영문 소문자와 숫자로만 입력해주세요.")
+    @field:Pattern(regexp = RegexExpression.USERNAME_RESERVED, message = "유저네임에 사용할 수 없는 단어가 포함되어 있습니다.")
     @field:Trimmed
     val username: String,
 
