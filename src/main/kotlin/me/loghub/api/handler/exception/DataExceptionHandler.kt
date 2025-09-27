3b2180b89dd86796a7140f2f7e01d5ae625c2fdb
gymynnym <gymynnym@gmail.com>
2025-09-27 21:27:01 +0900

    feat(global): Introduce `DataExceptionHandler`
    - Handling exceptions:
      - `EmptyResultDataAccessException` to `404`
      - `DataIntegrityViolationException` to `409`

---
+++ src/main/kotlin/me/loghub/api/handler/exception/DataExceptionHandler.kt
@@ -0,0 +1,28 @@
+package me.loghub.api.handler.exception
+
+import me.loghub.api.constant.message.ResponseMessage
+import me.loghub.api.dto.response.MessageResponseBody
+import me.loghub.api.dto.response.ResponseBody
+import org.springframework.dao.DataIntegrityViolationException
+import org.springframework.dao.EmptyResultDataAccessException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class DataExceptionHandler {
+    @ExceptionHandler(EmptyResultDataAccessException::class)
+    fun handleException(e: EmptyResultDataAccessException): ResponseEntity<ResponseBody> =
+        MessageResponseBody(
+            message = ResponseMessage.Default.NOT_FOUND,
+            status = HttpStatus.NOT_FOUND,
+        ).toResponseEntity()
+
+    @ExceptionHandler(DataIntegrityViolationException::class)
+    fun handleException(e: DataIntegrityViolationException): ResponseEntity<ResponseBody> =
+        MessageResponseBody(
+            message = ResponseMessage.Default.CONFLICT,
+            status = HttpStatus.CONFLICT,
+        ).toResponseEntity()
+}
