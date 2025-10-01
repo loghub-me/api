69407e05db76b98a6493be83c0003b49cf29c152
gymynnym <gymynnym@gmail.com>
2025-10-01 21:21:58 +0900

    test(article): Add ArticleCommentControllerTest

---
+++ src/test/kotlin/me/loghub/api/controller/article/ArticleCommentControllerTest.kt
@@ -0,0 +1,128 @@
+package me.loghub.api.controller.article
+
+import me.loghub.api.dto.article.comment.PostArticleCommentDTO
+import me.loghub.api.dto.auth.token.TokenDTO
+import me.loghub.api.service.test.TestGrantService
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.RequestEntity
+import org.springframework.http.ResponseEntity
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.jdbc.Sql
+import kotlin.test.assertEquals
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@Sql(
+    scripts = ["/database/data/truncate.sql", "/database/data/test.sql"],
+    executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS,
+)
+class ArticleCommentControllerTest(@Autowired private val rest: TestRestTemplate) {
+    companion object {
+        lateinit var member1: TokenDTO;
+        lateinit var member2: TokenDTO;
+
+        object ArticleCommentId {
+            const val FOR_GET_REPLIES = 1L
+            const val FOR_DELETE = 2L
+        }
+
+        val bodyForPost = PostArticleCommentDTO(content = "New comment", parentId = null)
+        val bodyForPostReply = PostArticleCommentDTO(content = "New reply", parentId = 1L)
+
+        @JvmStatic
+        @BeforeAll
+        fun setup(
+            @Autowired grantService: TestGrantService,
+            @Autowired rest: TestRestTemplate,
+        ) {
+            member1 = grantService.generateToken("member1")
+            member2 = grantService.generateToken("member2")
+        }
+    }
+
+    @Test
+    fun getComments() {
+        val response = getComments<String>(1L)
+        assertEquals(HttpStatus.OK, response.statusCode)
+    }
+
+    @Test
+    fun getReplies() {
+        val response = getReplies<String>(1L, ArticleCommentId.FOR_GET_REPLIES)
+        assertEquals(HttpStatus.OK, response.statusCode)
+    }
+
+    @Test
+    fun `postComment - unauthenticated`() {
+        val response = postComment<String>(1L, bodyForPost)
+        assertEquals(HttpStatus.UNAUTHORIZED, response.statusCode)
+    }
+
+    @Test
+    fun `postComment - ok`() {
+        val response = postComment<String>(1L, bodyForPostReply, member1)
+        assertEquals(HttpStatus.CREATED, response.statusCode)
+    }
+
+    @Test
+    fun `postReply - ok`() {
+        val response = postComment<String>(1L, bodyForPostReply, member1)
+        assertEquals(HttpStatus.CREATED, response.statusCode)
+    }
+
+    @Test
+    fun `deleteComment - unauthenticated`() {
+        val response = deleteComment<String>(1L, ArticleCommentId.FOR_DELETE)
+        assertEquals(HttpStatus.UNAUTHORIZED, response.statusCode)
+    }
+
+    @Test
+    fun `deleteComment - forbidden`() {
+        val response = deleteComment<String>(1L, ArticleCommentId.FOR_DELETE, member2)
+        assertEquals(HttpStatus.FORBIDDEN, response.statusCode)
+    }
+
+    @Test
+    fun `deleteComment - not found`() {
+        val response = deleteComment<String>(1L, 999L, member1)
+        assertEquals(HttpStatus.NOT_FOUND, response.statusCode)
+    }
+
+    @Test
+    fun `deleteComment - ok`() {
+        val response = deleteComment<String>(1L, ArticleCommentId.FOR_DELETE, member1)
+        assertEquals(HttpStatus.OK, response.statusCode)
+    }
+
+    private inline fun <reified T> getComments(articleId: Long) =
+        rest.getForEntity("/articles/${articleId}/comments", T::class.java)
+
+    private inline fun <reified T> getReplies(articleId: Long, commentId: Long) =
+        rest.getForEntity("/articles/${articleId}/comments/${commentId}/replies", T::class.java)
+
+    private inline fun <reified T> postComment(
+        articleId: Long,
+        body: PostArticleCommentDTO,
+        token: TokenDTO? = null
+    ): ResponseEntity<T> {
+        val request = RequestEntity.post("/articles/${articleId}/comments")
+        token?.let { request.header(HttpHeaders.AUTHORIZATION, it.authorization) }
+        return rest.exchange(request.body(body), T::class.java)
+    }
+
+    private inline fun <reified T> deleteComment(
+        articleId: Long,
+        commentId: Long,
+        token: TokenDTO? = null
+    ): ResponseEntity<T> {
+        val request = RequestEntity.delete("/articles/${articleId}/comments/${commentId}")
+        token?.let { request.header(HttpHeaders.AUTHORIZATION, it.authorization) }
+        return rest.exchange(request.build(), T::class.java)
+    }
+}
