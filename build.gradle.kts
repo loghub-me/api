de86d1b5988e28573c4e2147a02d0934fc445022
gymynnym <gymynnym@gmail.com>
2025-11-27 18:18:45 +0900

    fix: Resolve Hibernate and JPA warnings
    - Add equals and hashCode functions to `TrendingTopic` to resolve HHH000038, HHH000039
    - Disable `spring.jpa-open-in-view` for performance and security issue
    - Bump version to 0.4.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 group = "me.loghub"
 
-version = "0.4.0-SNAPSHOT"
+version = "0.4.1-SNAPSHOT"
 
 java { toolchain { languageVersion = JavaLanguageVersion.of(21) } }
 
--- src/main/kotlin/me/loghub/api/entity/topic/TrendingTopic.kt
+++ src/main/kotlin/me/loghub/api/entity/topic/TrendingTopic.kt
@@ -8,10 +8,21 @@ import org.hibernate.annotations.Immutable
 @Immutable // view
 class TrendingTopic(
     @Id
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
     @JoinColumn(name = "topic_id", nullable = false)
     val topic: Topic,
 
     @Column(name = "trending_score", nullable = false)
     val trendingScore: Double,
-)
+) {
+    override fun equals(other: Any?) = when {
+        this === other -> true
+        other !is TrendingTopic -> false
+        this.javaClass != other.javaClass -> false
+        topic.id == null || other.topic.id == null -> false
+        else -> this.topic.id == other.topic.id
+    }
+
+    override fun hashCode() = 31 * javaClass.hashCode() + (topic.id?.hashCode() ?: 0)
+
+}
