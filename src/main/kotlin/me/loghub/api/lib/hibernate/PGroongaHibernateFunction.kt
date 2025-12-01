7a83d5b786800cd1935001792ee12fc16df780fe
gymynnym <gymynnym@gmail.com>
2025-12-01 22:13:33 +0900

    fix(hibernate): Replace writer_username with topic_flats in pgroonga functions
    - Bump version to 0.4.4

--- src/main/kotlin/me/loghub/api/lib/hibernate/PGroongaHibernateFunction.kt
+++ src/main/kotlin/me/loghub/api/lib/hibernate/PGroongaHibernateFunction.kt
@@ -11,27 +11,27 @@ enum class PGroongaHibernateFunction(
 ) {
     ARTICLES_FTS(
         "articles_fts",
-        "ARRAY[title, content, writer_username] &@~ pgroonga_condition(" +
+        "ARRAY[title, content, topics_flat] &@~ pgroonga_condition(" +
                 "pgroonga_query_expand('search_synonyms', 'term', 'synonyms', ?1)," +
-                "weights => ARRAY[${WEIGHTS.TITLE}, ${WEIGHTS.CONTENT}, ${WEIGHTS.WRITER_USERNAME}]," +
+                "weights => ARRAY[${WEIGHTS.TITLE}, ${WEIGHTS.CONTENT}, ${WEIGHTS.TOPIC_FLATS}]," +
                 "index_name => 'articles_fts_idx')",
         1,
         StandardBasicTypes.BOOLEAN,
     ),
     SERIES_FTS(
         "series_fts",
-        "ARRAY[title, description, writer_username] &@~ pgroonga_condition(" +
+        "ARRAY[title, description, topics_flat] &@~ pgroonga_condition(" +
                 "pgroonga_query_expand('search_synonyms', 'term', 'synonyms', ?1)," +
-                "weights => ARRAY[${WEIGHTS.TITLE}, ${WEIGHTS.DESCRIPTION}, ${WEIGHTS.WRITER_USERNAME}]," +
+                "weights => ARRAY[${WEIGHTS.TITLE}, ${WEIGHTS.DESCRIPTION}, ${WEIGHTS.TOPIC_FLATS}]," +
                 "index_name => 'series_fts_idx')",
         1,
         StandardBasicTypes.BOOLEAN,
     ),
     QUESTIONS_FTS(
         "questions_fts",
-        "ARRAY[title, content, writer_username] &@~ pgroonga_condition(" +
+        "ARRAY[title, content, topics_flat] &@~ pgroonga_condition(" +
                 "pgroonga_query_expand('search_synonyms', 'term', 'synonyms', ?1)," +
-                "weights => ARRAY[${WEIGHTS.TITLE}, ${WEIGHTS.CONTENT}, ${WEIGHTS.WRITER_USERNAME}]," +
+                "weights => ARRAY[${WEIGHTS.TITLE}, ${WEIGHTS.CONTENT}, ${WEIGHTS.TOPIC_FLATS}]," +
                 "index_name => 'questions_fts_idx')",
         1,
         StandardBasicTypes.BOOLEAN,
@@ -48,7 +48,7 @@ enum class PGroongaHibernateFunction(
         const val TITLE = 5
         const val CONTENT = 1
         const val DESCRIPTION = 1
-        const val WRITER_USERNAME = 2
+        const val TOPIC_FLATS = 2
     }
 
     val template: String get() = "$funName(${(0 until parameterCount).joinToString(", ") { "{$it}" }})"
