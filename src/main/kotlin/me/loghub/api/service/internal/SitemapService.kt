d484e6751d935f4fb970fbb94712d27ae2153a89
gymynnym <gymynnym@gmail.com>
2026-01-04 16:45:35 +0900

    chore(sitemap): Increase articles and series priorities

--- src/main/kotlin/me/loghub/api/service/internal/SitemapService.kt
+++ src/main/kotlin/me/loghub/api/service/internal/SitemapService.kt
@@ -17,9 +17,9 @@ class SitemapService(
     private val topicRepository: TopicRepository,
 ) {
     private companion object {
-        const val ARTICLE_PRIORITY = 0.8
-        const val SERIES_PRIORITY = 0.8
-        const val SERIES_CHAPTER_PRIORITY = 0.8
+        const val ARTICLE_PRIORITY = 1.0
+        const val SERIES_PRIORITY = 1.0
+        const val SERIES_CHAPTER_PRIORITY = 1.0
         const val QUESTION_PRIORITY = 0.7
         const val TOPIC_PRIORITY = 0.6
     }
