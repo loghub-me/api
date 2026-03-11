a454711ac50d49a48b7cc1070ebe8a769a48adf2
gymynnym <gymynnym@gmail.com>
2026-03-11 15:29:39 +0900

    fix(notification) Add missing @Component to NotificationStreamEventListener

--- src/main/kotlin/me/loghub/api/listener/NotificationStreamEventListener.kt
+++ src/main/kotlin/me/loghub/api/listener/NotificationStreamEventListener.kt
@@ -2,9 +2,11 @@ package me.loghub.api.listener
 
 import me.loghub.api.dto.notification.event.NotificationEvent
 import me.loghub.api.service.notification.NotificationStreamService
+import org.springframework.stereotype.Component
 import org.springframework.transaction.event.TransactionPhase
 import org.springframework.transaction.event.TransactionalEventListener
 
+@Component
 class NotificationStreamEventListener(private val notificationStreamService: NotificationStreamService) {
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     fun handle(event: NotificationEvent) {
